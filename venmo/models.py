aa7a3b9dd7a47c9a3f5f30eef7a851131336af62
Max Fang <maxfangx@gmail.com>
2017-06-06 18:41:13 -0700

    Adapt models for Lightning Venmo

--- venmo/models.py
+++ venmo/models.py
@@ -1,163 +1,30 @@
-from django.db import models, transaction
-from django.db.models import F, Sum
+from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
-from venmo.helpers import partition_integer_by_weights
-
-# TODO create new type of UserException that can be exposed to the user
+from venmo.fields import BitcoinAddressField
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    upvote_balance = models.BigIntegerField(default=0, help_text="The balance from upvotes only")
-
-    def get_net_balance(self):
-        """
-        Returns the net balance of the account, taking deposits and
-        withdrawals into account
-        """
-        deposits = Deposit.objects.filter(user=self.user, status='accepted')
-        withdrawals = Withdrawal.objects.filter(user=self.user, status='executed')
-        deposit_total = sum(map(lambda d: d.amount, deposits))
-        withdrawal_total = sum(map(lambda w: w.amount, withdrawals))
-        return self.upvote_balance + deposit_total - withdrawal_total
-
-    def recalculate_upvote_balance(self):
-        # TODO Can insert validation checks here
-        pass
+    handle = models.CharField(max_length=30)
+    lightning_address = BitcoinAddressField()
 
 
-class Deposit(models.Model):
+class Payment(models.Model):
 
-    DEPOSIT_STATUS_CHOICES = (
-        ('received', 'Received'),
-        ('accepted', 'Accepted'),
+    PAYMENT_STATUS_CHOICES = (
+        ('pending_invoice', 'Pending Invoice'),
+        ('pending_payment', 'Pending Payment'),
+        ('complete', 'Complete'),
     )
+    status = models.CharField(max_length=50, default='accepted', choices=PAYMENT_STATUS_CHOICES)
 
-    user = models.ForeignKey(User)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    # TODO change default to received
-    status = models.CharField(max_length=50, default='accepted', choices=DEPOSIT_STATUS_CHOICES)
+    sender = models.ForeignKey(User)
+    recipient = models.ForeignKey(User)
     amount = models.IntegerField()
-
-
-class Withdrawal(models.Model):
-
-    WITHDRAWAL_STATUS_CHOICES = (
-        ('submitted', 'Submitted'),
-        ('approved', 'Approved'),
-        ('denied', 'Denied'),
-        ('executed', 'Executed'),
-    )
-
-    user = models.ForeignKey(User)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=50, default='submitted', choices=WITHDRAWAL_STATUS_CHOICES)
-    amount = models.IntegerField()
-
-
-class Post(models.Model):
-
-    POST_STATUS_CHOICES = (
-        ('visible', 'Visible'),
-        ('deleted by user', 'Deleted by user'),
-        ('deleted by admin', 'Deleted by admin'),
-    )
-
-    curator = models.ForeignKey(User)
-    title = models.CharField(max_length=300)
-    link = models.URLField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=50, default='visible', choices=POST_STATUS_CHOICES)
-
-    def score(self):
-        return sum(map(lambda u: u.amount, self.upvote_set.all()))
-
-    def __str__(self):
-        return "{} by {}".format(self.id, self.curator)
-
-    @property
-    def value(self):
-        qs = Upvote.objects.filter(post=self)
-        if qs.count() <= 0:
-            return 0
-        return qs.aggregate(value=Sum(F('amount'))).get('value', 0)
-
-    def upvote(self, user, amount):
-        existing_upvotes = Upvote.objects.filter(post=self).count()
-        Upvote(post=self,
-               user=user,
-               index=existing_upvotes + 1,
-               amount=amount).save()
-
-
-class Upvote(models.Model):
-
-    user = models.ForeignKey(User)
-    post = models.ForeignKey(Post)
-    index = models.IntegerField()
-    amount = models.IntegerField()
-    has_disbursed = models.BooleanField()
-
-    def save(self, *args, **kwargs):
-        if not self.has_enough_funds():
-            raise Exception("Not enough funds")
-        if self.amount < 0:
-            raise Exception("Insufficient amount")
-
-        if not self.pk:
-            self.has_disbursed = False
-
-        super(Upvote, self).save(*args, **kwargs)
-
-        if not self.has_disbursed:
-            self.disburse_tip()
-
-    def has_enough_funds(self):
-        if self.user.profile.get_net_balance() - self.amount > 0 and \
-                self.amount > Upvote.objects.filter(post=self.post, index__lt=self.index).count():
-            return True
-        else:
-            return False
-
-    def disburse_tip(self):
-        if self.has_disbursed:
-            raise Exception("Already disbursed")
-        if not self.has_enough_funds():
-            raise Exception("Not enough funds")
-
-        tipsters = Upvote.objects.filter(post=self.post, id__lt=self.id)
-
-        if tipsters.count() == 0:
-            # TODO take as fees
-            return
-
-        weights = {}
-        for tipster in tipsters:
-            weights[tipster.id] = tipster.amount
-
-        with transaction.atomic():
-            partition = partition_integer_by_weights(self.amount, weights)
-            upvoter_profile = self.user.profile
-            upvoter_profile.upvote_balance -= self.amount
-            upvoter_profile.save()
-            for tipster_id, share in partition.iteritems():
-                if tipster_id == 'remainder':
-                    continue
-                tipster_profile = tipsters.get(id=tipster_id).user.profile
-                tipster_profile.upvote_balance += share
-                tipster_profile.save()
-
-        self.has_disbursed = True
-        self.save()
-
+    status = models.CharField(max_length=50, default='accepted', choices=PAYMENT_STATUS_CHOICES)
 
 admin.site.register(Profile)
-admin.site.register(Deposit)
-admin.site.register(Withdrawal)
-admin.site.register(Post)
-admin.site.register(Upvote)
+admin.site.register(Payment)
